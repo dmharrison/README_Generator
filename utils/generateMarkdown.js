875137b44e9b7818e9e82e7398248db4a31f41ee
dmharrison <dmharrison2014@gmail.com>
2024-04-24 19:38:07 -0400

    added for renderlicense badge commented out license link because badges act as links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  let licenseBadge = "";
+  switch (license) {
+    case "MIT":
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    case "Apache 2.0 License":
+      licenseBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case "ISC":
+      licenseBadge = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+      break;
+    default:
+      licenseBadge = "";
+  }
+  return licenseBadge;
 }
 
-TODO: Create a function that returns the license link
-If there is no license, return an empty string
-function renderLicenseLink(license) {
-  
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+//Badges act as links
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -21,6 +35,7 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
  
+`;
 }
 
 module.exports = generateMarkdown;
